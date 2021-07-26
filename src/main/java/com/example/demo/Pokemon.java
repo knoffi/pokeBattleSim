84040e6e80942b8ae40a314ea323885b79043b52
knoffi <matthiastutor@outlook.de>
2021-07-26 17:06:35 +0200

    ADD #1 print name of pokemon nr. 7

---
+++ src/main/java/com/example/demo/Pokemon.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Pokemon {
+    public String name;
+
+    public Pokemon() {
+        this.name = "MissingNo";
+    }
+
+    public Pokemon(String name) {
+        this.name = name;
+    }
+}
