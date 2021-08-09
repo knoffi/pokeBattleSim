7592b1dcd951b43ead9487f41c2db71b3510ece9
knoffi <matthiastutor@outlook.de>
2021-08-09 23:59:13 +0200

    ADD #17 real duell from random teams by request

--- src/main/java/com/example/demo/Controller/DuellController.java
+++ src/main/java/com/example/demo/Controller/DuellController.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import com.example.demo.RequestMode;
 import com.example.demo.Pokedex.Pokedex;
 import com.example.demo.Pokemon.Pokemon;
+import com.example.demo.Searches.PokemonSearch.PokemonSearch;
 import com.example.demo.TrainerDuell.TrainerDuell;
 
 import org.springframework.stereotype.Controller;
@@ -17,10 +18,8 @@ public class DuellController {
     @GetMapping(value = "/getTrainerDuell")
     public @ResponseBody DuellLog index() {
         try {
-            Pokemon bluePokemon = Pokedex.getPokemon(10, RequestMode.JAVA_11).convert();
-            Pokemon redPokemon = Pokedex.getPokemon(15, RequestMode.JAVA_11).convert();
-            Pokemon[] blue = { bluePokemon, bluePokemon };
-            Pokemon[] red = { redPokemon };
+            Pokemon[] blue = this.getRandomPokemonTeam();
+            Pokemon[] red = this.getRandomPokemonTeam();
             LogPokemon[] blueLogPokemon = Arrays.stream(blue).map(pokemon -> pokemon.getLogData())
                     .toArray(LogPokemon[]::new);
             LogPokemon[] redLogPokemon = Arrays.stream(red).map(pokemon -> pokemon.getLogData())
@@ -32,5 +31,12 @@ public class DuellController {
             System.out.println(e);
         }
         return new DuellLog();
+    }
+
+    private Pokemon[] getRandomPokemonTeam() throws IOException, InterruptedException {
+        PokemonSearch[] pokemonSearches = Pokedex.getRandomTeam(RequestMode.JAVA_11);
+        Pokemon[] randomTeam = Arrays.stream(pokemonSearches).map(search -> search.convert()).toArray(Pokemon[]::new);
+        return randomTeam;
+
     }
 }
