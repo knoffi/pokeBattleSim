abf6319a4f650ede3042bf65d1e1a19792d65a1f
knoffi <matthiastutor@outlook.de>
2021-08-09 21:48:21 +0200

    ADD #17 return json conform to db.json

--- src/main/java/com/example/demo/Controller/DuellController.java
+++ src/main/java/com/example/demo/Controller/DuellController.java
@@ -7,15 +7,56 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DuellController {
     @GetMapping(value = "/getTrainerDuell")
-    public @ResponseBody Test index() {
-        return new Test("Hello");
+    public @ResponseBody DuellLog index() {
+        LogPokemon bluePokemon = new LogPokemon("ekans",
+                "https ://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/23.png",
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png");
+        LogPokemon redPokemon = new LogPokemon("magnemite",
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/81.png",
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/81.png");
+        String[] firstRoundText = { "Looks like we are in a Pokemon battle, M-M-Morty", "Oh geez, Rick!" };
+        LogRound firstRound = new LogRound("ekans", "magnemite", firstRoundText, true);
+        LogRound[] rounds = { firstRound };
+        LogPokemon[] blue = { bluePokemon, bluePokemon };
+        LogPokemon[] red = { redPokemon };
+        return new DuellLog(blue, red, rounds);
     }
 }
 
-class Test {
-    public String name;
+class DuellLog {
+    public LogPokemon[] blueTeam;
+    public LogPokemon[] redTeam;
+    public LogRound[] rounds;
 
-    Test(String name) {
+    DuellLog(LogPokemon[] blue, LogPokemon[] red, LogRound[] rounds) {
+        this.blueTeam = blue;
+        this.redTeam = red;
+        this.rounds = rounds;
+    }
+}
+
+class LogPokemon {
+    public String name;
+    public String backSprite;
+    public String frontSprite;
+
+    public LogPokemon(String name, String backSprite, String frontSprite) {
         this.name = name;
+        this.backSprite = backSprite;
+        this.frontSprite = frontSprite;
+    }
+}
+
+class LogRound {
+    public String redCombatant;
+    public String blueCombatant;
+    public String[] battleLog;
+    public boolean blueWon;
+
+    public LogRound(String redCombatant, String blueCombatant, String[] battleLog, boolean blueWon) {
+        this.redCombatant = redCombatant;
+        this.blueCombatant = blueCombatant;
+        this.battleLog = battleLog;
+        this.blueWon = blueWon;
     }
 }
