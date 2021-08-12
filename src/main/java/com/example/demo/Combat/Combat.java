3d0d525a594101688e2373fca7c777764192b2ec
knoffi <matthiastutor@outlook.de>
2021-08-12 19:26:15 +0200

    ADD #25 combat based on stats and exhaustion

--- src/main/java/com/example/demo/Combat/Combat.java
+++ src/main/java/com/example/demo/Combat/Combat.java
@@ -17,9 +17,11 @@ public class Combat {
     }
 
     public LogRound getResult() {
-        boolean blueWins = Math.random() < 0.5;
+        boolean blueWins = new BattleCalculation(this.blue, this.red).blueWins();
         Pokemon winner = blueWins ? this.blue : this.red;
         Pokemon loser = blueWins ? this.red : this.blue;
+
+        winner.addExhaustion();
 
         this.combatSummary.push(loser.getName() + " attacks with " + loser.getFinishingBlow());
         this.combatSummary.push(winner.getName() + " hits back with " + winner.getFinishingBlow());
@@ -29,4 +31,56 @@ public class Combat {
         return new LogRound(this.red.getName(), this.blue.getName(), combatSummary, blueWins);
     }
 
+}
+
+class BattleCalculation {
+    private Pokemon blue;
+    private Pokemon red;
+
+    public BattleCalculation(Pokemon blue, Pokemon red) {
+        this.blue = blue;
+        this.red = red;
+    }
+
+    public boolean blueWins() {
+        int blueVictoryPoints = 0;
+        blueVictoryPoints += this.blueStatBonus();
+        blueVictoryPoints += this.blueExhaustionBonus();
+        if (blueVictoryPoints > 0) {
+            return true;
+        } else if (blueVictoryPoints < 0) {
+            return false;
+        } else {
+            return this.blueWinsRandomly();
+        }
+    }
+
+    private int blueExhaustionBonus() {
+        int blueExhaustioDiff = this.blue.getExhaustion() - this.red.getExhaustion();
+        boolean blueIsExhausted = blueExhaustioDiff > 0;
+        if (blueIsExhausted) {
+            return -(int) Math.pow(2, blueExhaustioDiff);
+        } else {
+            boolean redIsExhausted = blueExhaustioDiff < 0;
+            if (redIsExhausted) {
+                return (int) Math.pow(2, -blueExhaustioDiff);
+            }
+        }
+        return 0;
+    }
+
+    private boolean blueWinsRandomly() {
+        boolean blueWins = Math.random() < 0.5;
+        return blueWins;
+    }
+
+    private int blueStatBonus() {
+        return this.blueSumDiff();
+    }
+
+    private int blueSumDiff() {
+        int sumRed = this.red.getStatSum();
+        int sumBlue = this.blue.getStatSum();
+        return (int) Math.round((sumBlue - sumRed) / 15);
+    }
 }
