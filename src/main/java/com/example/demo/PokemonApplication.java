41d9eb49f3dda95b1183f13d1a8497d84e8816e9
knoffi <matthiastutor@outlook.de>
2021-08-22 21:24:52 +0200

    ADD #36 google translate api call

--- src/main/java/com/example/demo/PokemonApplication.java
+++ src/main/java/com/example/demo/PokemonApplication.java
@@ -1,26 +1,35 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PokemonApplication {
 
 	public static void main(String[] args) {
-		// try {
-		// PokemonSearch[] test = Pokedex.getRandomTeam(RequestMode.JAVA_11);
-		// for (PokemonSearch search : test) {
-		// System.out.println(search.name);
-		// }
-		// } catch (IOException | InterruptedException e) {
-		// System.out.println("error");
-		// }
-		SpringApplication.run(PokemonApplication.class, args);
-		// System.out.println(TypeStore.getEffectiveness("normal", "fighting").value);
-		// System.out.println(TypeStore.getEffectiveness("normal", "ghost").value);
-		// System.out.println(TypeStore.getEffectiveness("fire", "grass").value);
-		// System.out.println(TypeStore.getEffectiveness("fire", "water").value);
-		// System.out.println(TypeStore.getEffectiveness("ice", "water").value);
+
+		// SpringApplication.run(PokemonApplication.class, args);
+		try {
+			Translate translate = TranslateOptions.newBuilder()
+					.setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream(
+							"C:/Users/monop/programming/pokeFightApi/jsons/ace-case-323614-5db3d27519e9.json")))
+					.build().getService();
+
+			Translation translation = translate.translate("¡Hola Mundo!",
+					Translate.TranslateOption.sourceLanguage("es"), Translate.TranslateOption.targetLanguage("de"),
+					Translate.TranslateOption.model("base"));
+			System.out.printf("Translated Text:\n\t%s\n", translation.getTranslatedText());
+		} catch (IOException e) {
+			System.out.println(e.getClass());
+		}
+		;
 	}
 
 }
