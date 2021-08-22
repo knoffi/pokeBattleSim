2d1a111fab60557ef47422b645fcd1de5ad79800
knoffi <matthiastutor@outlook.de>
2021-08-22 23:58:32 +0200

    ADD #36 several keys for translation query

--- src/main/java/com/example/demo/Translater/TranslationKeyMapper.java
+++ src/main/java/com/example/demo/Translater/TranslationKeyMapper.java
@@ -8,6 +8,13 @@ class TranslationKeyMapper {
     static {
         keyMap.put("en", "en");
         keyMap.put("de", "de");
+        keyMap.put("ko", "ko");
+        keyMap.put("fr", "fr");
+        keyMap.put("es", "es");
+        keyMap.put("it", "it");
+        keyMap.put("zh", "zh-hant");
+        keyMap.put("zh-hant", "zh-hant");
+        keyMap.put("ja", "ja");
     }
 
     public static String getKey(String languageParam) {
