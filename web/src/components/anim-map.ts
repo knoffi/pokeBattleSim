3dbd91f14a7e636277bb63605af14e2438246857
Mirco Kraenz <kraenzmirco@gmail.com>
2021-08-30 15:48:41 +0200

    WIP #52 design battle log for animatability
    outline: how to map attack type to animation
    
    Co-authored-by: knoffi <knoffi@users.noreply.github.com>

---
+++ web/src/components/anim-map.ts
@@ -0,0 +1,30 @@
+// TODO use me
+// const attackTypeToEffectMap = {
+//     water: "water-effect",
+//     electric: "electric-effect",
+//     grass: "grass-effect",
+//     fire: "fire-effect",
+//     bug: "bug-effect",
+//     normal: "normal-effect",
+//     fighting: "fighting-effect",
+//     psychic: "psychic-effect",
+//     flying: "flying-effect",
+//     poison: "poison-effect",
+//     ground: "ground-effect",
+//     rock: "rock-effect",
+//     ghost: "ghost-effect",
+//     dragon: "dragon-effect",
+//     ice: "ice-effect",
+
+//     statusChange: "status-change-effect",
+
+//     dark: "dark-effect",
+//     steel: "steel-effect",
+//     fairy: "fairy-effect",
+// };
+
+// const event: any = {};
+// const effect =
+//     attackNameToEffectMap[event.attackName] ||
+//     attackTypeToEffectName[event.attackType] ||
+//     defaultAttackEffect;
