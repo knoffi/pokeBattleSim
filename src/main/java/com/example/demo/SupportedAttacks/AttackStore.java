b0813625a32d622b9876dd92115d870177017cfe
knoffi <matthiastutor@outlook.de>
2021-09-09 13:24:20 +0200

    ADD #55 get updated move names from attack store

--- src/main/java/com/example/demo/SupportedAttacks/AttackStore.java
+++ src/main/java/com/example/demo/SupportedAttacks/AttackStore.java
@@ -1,7 +1,12 @@
 package com.example.demo.SupportedAttacks;
 
+import java.io.IOException;
 import java.util.Arrays;
 
+import com.example.demo.RequestMode;
+import com.example.demo.Pokedex.Pokedex;
+import com.example.demo.Searches.MoveSearch.MoveSearch;
+import com.example.demo.Searches.MovesSearch.MovesSearch;
 import com.example.demo.StoreButler.StoreButler;
 import com.example.demo.StoreButler.StoreButlerServices;
 
@@ -24,14 +29,46 @@ public class AttackStore {
         return Arrays.stream(SUPPORTED_ATTACKS_BY_NAME).anyMatch(attackName -> attackName.equals(name));
     }
 
+    public static String[] getUpdatedAttackNames() {
+        try {
+            MovesSearch relevantMoves = Pokedex.getPokeData(Pokedex.API_PATH + Pokedex.RELEVANT_ATTACKS,
+                    MovesSearch.class, RequestMode.JAVA_11);
+            String[] attacks = Arrays.stream(relevantMoves.results).map(move -> getMoveFromURL(move.url))
+                    .filter(move -> GenerationFilter.check(move.generation.name))
+                    .filter(move -> CategoryFilter.check(move.meta.category.name)).map(moveSearch -> moveSearch.name)
+                    .toArray(String[]::new);
+            return attacks;
+
+        } catch (IOException | InterruptedException e) {
+            System.out.print("___FAIL AT GETTING RELEVANT MOVES___");
+        }
+        ;
+        String[] defaultResult = { "struggle" };
+        return defaultResult;
+    }
+
+    private static MoveSearch getMoveFromURL(String url) {
+        try {
+            return Pokedex.getPokeDataByURL(url, MoveSearch.class, RequestMode.JAVA_11);
+        } catch (IOException | InterruptedException e) {
+            System.out.println("___FAIL AT GETTING MOVE " + url + "___");
+            return new MoveSearch();
+        }
+
+    }
 }
 
-class SupportedMoveCategories {
+class CategoryFilter {
     private static String[] categories = { "damage", "damage+ailment", "damage+lower", "damage+raise",
             "net-good-stats" };
 
-    public static boolean contain(String testCategory) {
-        boolean testIsSupported = Arrays.stream(categories).anyMatch(category -> category.equals(testCategory));
-        return testIsSupported;
+    public static boolean check(String testCategory) {
+        return Arrays.stream(categories).anyMatch(category -> category.equals(testCategory));
     };
+}
+
+class GenerationFilter {
+    public static boolean check(String generation) {
+        return generation.equals("generation-i");
+    }
 }
