5377c3bd5f71e596411033a2b59c8496200d7739
knoffi <matthiastutor@outlook.de>
2021-09-15 23:52:21 +0200

    ADD #51 status construction by string

--- src/main/java/com/example/demo/Pokemon/Status/Status.java
+++ src/main/java/com/example/demo/Pokemon/Status/Status.java
@@ -17,4 +17,63 @@ public class Status {
         this.damageReduce = 0;
 
     }
+
+    public Status(String name) {
+        boolean statusWasNotFound = true;
+        if (name.equals(StatusKeys.BURN.name)) {
+            this.key = StatusKeys.NONE;
+            this.roundsLeft = 300;
+            this.dotDamage = 0.05;
+            this.selfHarm = 0;
+            this.damageReduce = 0.05;
+            statusWasNotFound = false;
+        }
+        if (name.equals(StatusKeys.POISON.name)) {
+            this.key = StatusKeys.NONE;
+            this.roundsLeft = 300;
+            this.dotDamage = 0.1;
+            this.selfHarm = 0;
+            this.damageReduce = 0;
+            statusWasNotFound = false;
+        }
+        if (name.equals(StatusKeys.CONFUS.name)) {
+            this.key = StatusKeys.NONE;
+            this.roundsLeft = 3;
+            this.dotDamage = 0;
+            this.selfHarm = 0.1;
+            this.damageReduce = 0;
+            statusWasNotFound = false;
+        }
+        if (name.equals(StatusKeys.SLEEP.name)) {
+            this.key = StatusKeys.NONE;
+            this.roundsLeft = 3;
+            this.dotDamage = 0;
+            this.selfHarm = 0;
+            this.damageReduce = 1;
+            statusWasNotFound = false;
+        }
+        if (name.equals(StatusKeys.FREEZE.name)) {
+            this.key = StatusKeys.NONE;
+            this.roundsLeft = 3;
+            this.dotDamage = 0;
+            this.selfHarm = 0;
+            this.damageReduce = 1;
+            statusWasNotFound = false;
+        }
+        if (name.equals(StatusKeys.PARA.name)) {
+            this.key = StatusKeys.NONE;
+            this.roundsLeft = 3;
+            this.dotDamage = 0;
+            this.selfHarm = 0;
+            this.damageReduce = 1;
+            statusWasNotFound = false;
+        }
+        if (statusWasNotFound) {
+            try {
+                throw new Exception("PokeStatusNotFound");
+            } catch (Exception e) {
+                System.out.println("___STATUS " + name + " WAS NOT FOUND");
+            }
+        }
+    }
 }
