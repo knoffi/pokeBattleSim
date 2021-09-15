001f994165731cbe468079b77a9cce547e65a82f
knoffi <matthiastutor@outlook.de>
2021-09-15 23:51:22 +0200

    ADD #51 freeze ailment

--- src/main/java/com/example/demo/Pokemon/Status/StatusKeys.java
+++ src/main/java/com/example/demo/Pokemon/Status/StatusKeys.java
@@ -1,7 +1,8 @@
 package com.example.demo.Pokemon.Status;
 
 public enum StatusKeys {
-    PARA("paralysis"), SLEEP("sleep"), BURN("burn"), POISON("poison"), CONFUS("confusion"), NONE("none");
+    PARA("paralysis"), SLEEP("sleep"), BURN("burn"), POISON("poison"), CONFUS("confusion"), FREEZE("freeze"),
+    NONE("none");
 
     public final String name;
 
